d2302897443562d47c2317f21953fc501b62bd4d
Cosmin Adrian <cosminadrian218@gmail.com>
2022-05-17 15:24:39 +0300

    Style header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,12 +3,14 @@ import { NavLink } from 'react-router-dom';
 import logo from '../../logo.png';
 
 const Header = () => (
-  <header>
-    <NavLink to="/">
-      <img src={logo} alt="logo" />
-      {'Space Travelers\' Hub'}
+  <header className="h-16 w-[calc(100% - 6rem)] flex flex-row justify-between items-center my-6 mx-12 border-b-2 border-black pb-4">
+    <NavLink className="h-full flex flex-row gap-3 justify-between items-center" to="/">
+      <img className="h-full" src={logo} alt="logo" />
+      <span className="text-3x1">
+        Space Travelers&apos; Hub
+      </span>
     </NavLink>
-    <nav>
+    <nav className="flex flex-row gap-3 justify-between items-center">
       <NavLink to="/">Rockets</NavLink>
       <NavLink to="/mission">Mission</NavLink>
       <hr />
