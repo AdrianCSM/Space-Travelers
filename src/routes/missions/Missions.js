4210dde578926a14fe01ebff4a86436364d270fe
Cosmin Adrian <cosminadrian218@gmail.com>
2022-05-19 03:00:56 +0300

    Change file path

--- src/routes/missions/Missions.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Missions = () => <h1> These my missions </h1>;
-
-export default Missions;
--- src/routes/rockets/Rockets.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Rockets = () => (
-  <h1>
-    Ready for launch
-  </h1>
-);
-
-export default Rockets;
