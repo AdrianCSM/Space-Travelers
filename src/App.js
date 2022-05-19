2adbb0505e249cbc7805ea846fc78971610bc12a
Alfred <kalumunabisimwa5@gmail.com>
2022-05-19 11:26:18 +0200

    mission add api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Header from './components/header/Header';
-import Rockets from './routes/rockets/Rockets';
+import Navbar from './components/header/Header';
+import Missions from './routes/missions/missions';
 import Profile from './routes/profile/Profile';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Rockets />} />
+        <Route path="/" element={<Missions />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
